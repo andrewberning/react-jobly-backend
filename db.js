4b8253e387f2a0a9442026f3298cd9cf8ec41528
andrewberning <berningan89@gmail.com>
2024-05-26 16:22:15 -0700

    modified DB_URI

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ let DB_URI;
 if (process.env.NODE_ENV === "test") {
   DB_URI = "postgresql:///jobly_test";
 } else {
-  DB_URI = "postgresql:///jobly";
+  DB_URI = "postgres://pvwplfai:2Hos-4qjcGP3RcgCUVV-TXu5Zgs_tEsq@bubble.db.elephantsql.com/pvwplfai";
 }
 
 let db = new Client({
